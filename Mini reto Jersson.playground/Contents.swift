1392474cc6bd289a521e605b00cd2feec86bede0
MacBook Pro Carrales Portillo <macbookpro1@macbook-pro.local>
2016-03-02 14:10:26 -0600

    rev1

---
+++ Mini reto Jersson.playground/Contents.swift
@@ -0,0 +1,39 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+for num in 1...100 {
+
+    
+    
+    if num % 2 == 1 {
+        
+        print ("\(num) Impar!!!")
+        
+        }
+
+    
+    if num % 2 == 0 {
+        
+        print ("\(num) Par!!!")
+
+        }
+
+    
+    if num % 5 == 0 {
+        
+        print ("\(num) Bingo!!!")
+        
+    }
+
+    
+    if num >= 30 && num <= 40 {
+        
+        print ("\(num) Viva Swift!!!")
+    
+    }
+
+
+}
+
